3ea05b6019b8664c02608ef70a4bb739b08423dc
Yaroslav <skorpion.425358@gmail.com>
2019-11-04 14:10:47 +1000

    AtackEnemySystem[Test]
    -added primary atack

--- Scripts/Enemy00.gd
+++ Scripts/Enemy00.gd
@@ -1,14 +1,20 @@
 extends RigidBody2D
 
+const PrimaryAtack = preload("res://Scenes/PrimaryAtack.tscn")
+
 export var min_speed = 150
 export var max_speed = 250
 
 func _ready():
+	$EnemyAtackTimer.start()
 	pass 
-
-func _on_start_game():
-	queue_free()
 
 func _on_Visible_screen_exited():
 	queue_free()
+	pass
+
+func _on_EnemyAtackTimer_timeout():
+	var atack = PrimaryAtack.instance()
+	add_child(atack)
+	atack.position = $EnemyPos.position
 	pass
