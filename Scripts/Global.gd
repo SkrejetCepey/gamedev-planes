8bc4952c440ed398b9640a7a876d1ab34387868f
Yaroslav <skorpion.425358@gmail.com>
2019-11-09 22:11:45 +1000

    Test functional UI system

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,10 +3,9 @@ extends Control
 var current_scene = null
 
 func _ready():
-	
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
-	pass # Replace with function body.
+	pass
 
 func goto_scene(path):
 	call_deferred("_deferred_goto_scene", path)
@@ -18,6 +17,10 @@ func _deferred_goto_scene(path):
 	get_tree().get_root().add_child(current_scene)
 	get_tree().set_current_scene(current_scene)
 
-func _on_Button_pressed():
-	#Global.goto_scene("res://GUI.tscn")
-	pass # Replace with function body.
+func _on_ButtonPlayerTest_pressed():
+	#next time maybe :C
+	pass 
+
+func _on_ButtonEnemyTest_pressed():
+	Global.goto_scene("res://Scenes/EnemyTestLocation.tscn")
+	pass 
