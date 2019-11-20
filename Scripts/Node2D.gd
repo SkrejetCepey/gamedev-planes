2237ba7f9ba4e26288893c6166a225f0e270d15d
Dok1959 <dok1959.good@mail.ru>
2019-11-21 02:37:41 +1000

    Added Best Bullets1.1

--- Scripts/Node2D.gd
+++ Scripts/Node2D.gd
@@ -5,9 +5,4 @@ func _ready():
 	var bull = bullet.instance()
 	add_child(bull)
 	bull.initialize(Vector2(100,100),Vector2(0,1),180,300,500)
-	var bull1 = bullet.instance()
-	add_child(bull1)
-	bull1.initialize(Vector2(200,100),Vector2(0,1),180,300,800)
-	var pl = Player.instance()
-	add_child(pl)
 	pass
