20ded4621763b08364d3c8a4a82358cb1de18ece
Animark <ditren14@gmail.com>
2019-11-21 22:19:35 +1000

    PlayerHealthChanges

--- Scripts/Plane.gd
+++ Scripts/Plane.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 const Rocket = preload("res://Scenes/PlayerProjectileRocket.tscn")
 const Maingun = preload("res://Scenes/PlayerCannon.tscn")
-var health = 3 
+var health = 150 
 
 func _ready():
 	add_to_group("player")
 	pass 
+
+func _physics_process(delta):
+	print(health)
+	if health <= 0:
+		get_parent().self_destruct()
+	pass
