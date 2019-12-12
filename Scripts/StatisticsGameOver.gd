8c9ffe2ca4e3937cfac07a3a131b0cf6f826ff78
Yaroslav <skorpion.425358@gmail.com>
2019-12-13 02:37:33 +1000

    +Bugreport

--- Scripts/StatisticsGameOver.gd
+++ Scripts/StatisticsGameOver.gd
@@ -10,3 +10,7 @@ func _on_Button_Ok_pressed():
 func _on_Button_continue_pressed():
 	get_tree().change_scene("res://Scenes/ArcadeScene.tscn")
 	pass 
+
+func _on_Button_bugreport_pressed():
+	OS.shell_open("mailto:sk0rpion.425358@gmail.com")
+	pass
